ad195c1d28cc608901859ec473cb924be0c19a7e
Jason Gauthier <jagauthier@gmail.com>
2017-04-26 16:11:24 -0400

    Made the test application take parameters for easier testing.

--- test_webhook.py
+++ test_webhook.py
@@ -4,29 +4,12 @@ import string
 import random
 import time
 import requests
-
-# Set a location
-latitude = 39.655361
-longitude = -84.225871
-
-# generate this token with ./whserver -g <name>
-TOKEN = 'TOKEN HERE'
-WEBHOOK_URL = 'http://localhost:6009/' + TOKEN
-
-# set this to the number of iterations of data to generate
-# Run this in the background multiple times for load testing
-ITERATIONS = 1
-
-# You can run this for a specific amount of minutes or iterations.
-# whichever comes first
-MINUTES = 3
-
-variance = .20000
+from optparse import OptionParser
 
 UNOWN = 201
 
 
-def get_pokemon():
+def get_pokemon(options):
 
     lst = [random.choice(
            string.ascii_letters + string.digits) for n in xrange(16)]
@@ -34,9 +17,9 @@ def get_pokemon():
     spawnpoint = [random.choice(
         string.ascii_letters + string.digits) for n in xrange(12)]
     spawnpoint = "".join(spawnpoint)
-
     despawn = random.randint(15 * 60, 60 * 60)
-
+    location = options.location.split(",")
+    variance = options.variance
     pokemon = {
         'encounter_id': encounter,
         'spawnpoint_id': spawnpoint,
@@ -47,8 +30,10 @@ def get_pokemon():
         'individual_defense': random.randint(0, 15),
         'individual_stamina': random.randint(0, 15),
         'last_modified_time': int(time.time() * 1000) + 3600,
-        'latitude': latitude + random.uniform(0 - variance, variance),
-        'longitude': longitude + random.uniform(0 - variance, variance),
+        'latitude': float(
+            location[0]) + random.uniform(0 - variance, variance),
+        'longitude': float(
+            location[1]) + random.uniform(0 - variance, variance),
         'move_1': random.randint(1, 137),
         'move_2': random.randint(200, 281),
         'pokemon_id': random.randint(1, 251),
@@ -65,54 +50,22 @@ def get_pokemon():
     return pokemon
 
 
-def get_gym_pokemon():
-
-    lst = [random.choice(
-           string.ascii_letters + string.digits) for n in xrange(16)]
-    encounter = b64encode("".join(lst))
-    spawnpoint = [random.choice(
-        string.ascii_letters + string.digits) for n in xrange(12)]
-    spawnpoint = "".join(spawnpoint)
-
-    despawn = random.randint(15 * 60, 60 * 60)
-
-    pokemon = {
-        'encounter_id': encounter,
-        'spawnpoint_id': spawnpoint,
-        'disappear_time': int(time.time()) + despawn,
-        'gender': random.randint(1, 2),
-        'height': random.random(),
-        'individual_attack': random.randint(0, 15),
-        'individual_defense': random.randint(0, 15),
-        'individual_stamina': random.randint(0, 15),
-        'last_modified_time': int(time.time() * 1000) + 3600,
-        'latitude': latitude,
-        'longitude': longitude,
-        'move_1': random.randint(1, 281),
-        'move_2': random.randint(1, 281),
-        'pokemon_id': random.randint(1, 251),
-        'seconds_until_despawn': despawn,
-        'spawn_start': random.randint(500, 3000),
-        'spawn_end': random.randint(500, 3000),
-        'time_until_hidden_ms': int(time.time() * 1000) + 7200,
-        'verified': True,
-        'weight': random.uniform(5, 15)
-    }
-    return pokemon
-
-
-def get_pokestop():
+def get_pokestop(options):
 
     lst = [random.choice(string.ascii_letters + string.digits)
            for n in xrange(16)]
     id = b64encode("".join(lst))
+    location = options.location.split(",")
+    variance = options.variance
     pokestop = {
         'last_modified_time':  int(time.time() +
                                    random.randint(-3600, 0)) * 1000,
         'lure_expiration': None,
         'active_fort_modifier': None,
-        'latitude': latitude + random.uniform(0 - variance, variance),
-        'longitude': longitude + random.uniform(0 - variance, variance),
+        'latitude': float(
+            location[0]) + random.uniform(0 - variance, variance),
+        'longitude': float(
+            location[1]) + random.uniform(0 - variance, variance),
         'enabled': True,
         'pokestop_id': id
     }
@@ -166,62 +119,86 @@ def get_gymdetails(gym):
     return gymdetails
 
 
-def get_gym():
+def get_gym(options):
     lst = [random.choice(string.ascii_letters + string.digits)
            for n in xrange(16)]
     id = b64encode("".join(lst))
+    location = options.location.split(",")
+    variance = options.variance
     gym = {
         'gym_id': id,
         'team_id': random.randint(1, 3),
         'guard_pokemon_id': random.randint(1, 251),
         'gym_points': random.randint(1000, 50000),
         'enabled': 1,
-        'latitude': latitude + random.uniform(0 - variance, variance),
-        'longitude': longitude + random.uniform(0 - variance, variance),
+        'latitude': float(
+            location[0]) + random.uniform(0 - variance, variance),
+        'longitude': float(
+            location[1]) + random.uniform(0 - variance, variance),
         'last_modified': int(time.time() + random.randint(-3600, 0)) * 1000
     }
     return gym
 
 
-def process_post(data):
+def process_post(message, payload, options):
 
-    webhook_url = WEBHOOK_URL
-    requests.post(webhook_url, json=data, headers={
+    data = {
+        'type': message,
+        'message': payload
+    }
+
+    requests.post(options.webhook_url, json=data, headers={
                   'Content-Type': 'application/json'})
 
 
 if __name__ == '__main__':
+
+    parser = OptionParser()
+
+    parser.add_option("-i", "--iterations", type="int",
+                      dest="iterations", default=1,
+                      help="Iterations to process.")
+
+    parser.add_option("-m", "--minutes",
+                      dest="minutes", default=1, type="int",
+                      help="Minutes to run.")
+
+    parser.add_option("-w", "--webhook", dest="webhook_url",
+                      help="Webhook url")
+
+    parser.add_option("-l", "--location", dest="location",
+                      help="Location")
+
+    parser.add_option("-v", "--variance", dest="variance",
+                      default=.2000,
+                      help="Location variance")
+
+    (options, args) = parser.parse_args()
+
+    if not options.webhook_url or not options.location:
+        print "Webhook url and location are required."
+        exit(0)
+
     start = time.time()
-    for i in range(0, ITERATIONS):
-        if time.time() - start > MINUTES * 60:
+    for i in range(0, options.iterations):
+        if time.time() - start > options.minutes * 60:
             break
-        pokemon = get_pokemon()
-        data = {
-            'type': 'pokemon',
-            'message': pokemon
-        }
-        process_post(data)
+        pokemon = get_pokemon(options)
+        process_post('pokemon', pokemon, options)
+
+        # below here, these are pokestop, and gyms.
+        # The intent is that when you run this for thousands
+        # you will only get some of these.
+
         if random.randint(0, 1000) > 990:
-            pokestop = get_pokestop()
-            data = {
-                'type': 'pokestop',
-                'message': pokestop
-            }
-            process_post(data)
+            pokestop = get_pokestop(options)
+            process_post('pokestop', pokestop, options)
 
         if random.randint(0, 1000) > 999:
-            gym = get_gym()
+            gym = get_gym(options)
             gymdetails = get_gymdetails(gym)
-            data = {
-                'type': 'gym',
-                'message': gym
-            }
-            process_post(data)
-            data = {
-                'type': 'gym_details',
-                'message': gymdetails
-            }
-            process_post(data)
+            process_post('gym', gym, options)
+            process_post('gymdetails', gymdetails, options)
 
         # just a gentle pause
         time.sleep(.05)
